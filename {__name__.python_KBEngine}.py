ad32747e82c2bdd38ba3a5abb658b7cbaebf8b09
IVANPC <523166477@qq.com>
2019-01-12 11:53:34 +0800

    Python_KBEngine

---
+++ {__name__.python_KBEngine}.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import KBEngine
+from KBEDebug import *
+
+
+class {__name__}(KBEngine.Entity):
+    """
+    实体的base部分的实现
+    """
+    def __init__(self):
+        KBEngine.Entity.__init__(self)
